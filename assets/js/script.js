bc8c876410cf965242165e2cecdbe8a8d4feb74f
Your Name <you@example.com>
2022-04-24 14:48:14 +0100

    arrays for characters included in password added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,96 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+// array of lowercase characters for the password
+const lowerCaseCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// array of uppercase characters for the password
+const upperCaseCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+// array of numerical characters for the password
+const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// array of special characters for the password
+const specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
 const getPasswordLength = () => {
   return 10;
 };
@@ -15,6 +105,9 @@ const getPasswordCriteria = () => {
 };
 
 const createRandomPassword = () => {
+  // prompt user for length
+
+  //
   return "kdUE28(@d0";
 };
 
